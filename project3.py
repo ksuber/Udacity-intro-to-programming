7a9bf7544a9715b65bd4271e6dd455650d30e9c2
ksuber <k_suber52@yahoo.com>
2018-01-18 20:28:03 -0500

    Update project3.py
    final working copy

--- project3.py
+++ project3.py
@@ -1,6 +1,6 @@
 import textwrap  # to wrap text printed to screen evenly
 
-#Questions for quiz list of list
+# Questions for quiz list of list
 question_bank = [['easy', """The original WU members are __A__, GZA, __B__ Man, Raekwon, __C__ Killah,
 Inspectah Deck, __D__ God, Masta Killa, Ol Dirty Bastard and ocassionally __E__."""],
                  ['normal', """It had always been planned for __A__ Man to be the first breakout star from the groups lineup
@@ -53,57 +53,50 @@ def word_in_pos(word, answer_keys):
 
 
 def redraw_board(text, replace_keys):
-    #redraws the board updated withh a correct answer 
+    # redraws the board updated withh a correct answer
     for key, answer in replace_keys:
         text = text.replace(key, answer.title())
     print "*" * 100 + "\n" + textwrap.fill(text, 75) + '\n' + "*" * 100
 
 
+def wrong_answer(answer):
+    print "\n\nIncorrect Answer Try Again"
+    user_input = raw_input("Input an Answer for " + answer + " :")
+    return user_input
+
+
+def correct_answer(word, answer, answers, redrawn_board, replaced):
+    new_board = []
+    print "Great! Correct Answer\n\n"
+    popped = answers.pop()
+    word = word.replace(answer, popped)
+    replacement_key = (answer, word)
+    new_board.append(replacement_key)
+    redraw_board(redrawn_board, new_board)
+    word = word.replace(answer, popped)
+    return replaced.append(word)
+
+
 def play_game(game_board, answer_keys, answers):
     """ Game engine takes a board of questions plus answer and keys.
-    splits questions into words to check for replacement and reassembles 
+    splits questions into words to check for replacement and reassembles
      reprompts on incorrect and calls redraw_board on correct respones"""
-    count = 0
-    replaced = []
+    count, replaced = 0, []
     game_board = game_board.split()
     re_board = " ".join(game_board)
-    new_board = []
     for word in game_board:
         answer = word_in_pos(word, answer_keys)
         if answer is not None:
             user_input = raw_input("Input an Answer for " + answer + " : ")
             while user_input != answers[-1]:
                 if count <= 3:
-                    print "\n\nIncorrect Answer Try Again"
+                    user_input = wrong_answer(answer)
                     count += 1
-                    user_input = raw_input("Input an Answer for " + answer + " :")
-
                 else:
-                    print "Sorry You Loose"
-                    return None
-
+                    print "\n\nSorry You Loose"
+                    return
             else:
-                print "Great! Correct Answer\n\n"
-                popped = answers.pop()
-                word = word.replace(answer, popped)
-                replaced.append(word)
-                replacement_key = (answer, word)
-                new_board.append(replacement_key)
-                redraw_board(re_board, new_board)
-
+                correct_answer(word, answer, answers, re_board, replaced)
         else:
             replaced.append(word)
-    replaced = " ".join(replaced)
-    print replaced
-
-# print question_bank[1][0]
-
-
-print "*" * 100
-user_difficulty = raw_input("""Welcome to 36 chambers. To start game begin by typing
-a difficulty level. Acceptable choices are easy, normal,and hard: """)
-
-
-game_dificulty(user_difficulty, levels)
-gb, answers = game_board(user_difficulty, question_bank, answers)
-play_game(gb, answer_keys, answers)
+    print "YOU WIN!!!\n\n" + " ".join(replaced)
