247672902edac4a0dad956afd4db3ac888b5001c
ksuber <k_suber52@yahoo.com>
2018-01-22 21:33:11 -0500

    Add files via upload

---
+++ project4/movie_site.py
@@ -0,0 +1,32 @@
+import fresh_tomatoes
+import videos
+
+
+shaolin_vs_wutang = videos.Movies("Shaolin vs Wu Tang", " The film is about the rivalry \
+between the martial arts schools Shaolin and Wu Tang. It is also called Shaolin and Wu-Tang in the Master Killer Collection.",
+                                  "1 hour 27 min", "https://www.youtube.com/watch?v=eN2uwv96tHw", "https://img.fstatic.com/w156DTY4vVozZKjgl-Iipn_\
+VhRs=/fit-in/290x478/smart/https://cdn.fstatic.com/media/movies/covers/2010/12/7b013bf7c5d1d3293f74004ff903652f.jpg")
+
+mystery_of_chessboxing = videos.Movies("Mystery of Chessboxing", "Lee Yi Min stars as a young boy,\
+ Ah Pao, who wants to learn kung fu so that he can avenge his father's death at the hands of the \
+  Ghost Faced Killer ", "1h 30min", "https://www.youtube.com/watch?v=R2fgGM4Y71U", "https://i.pinimg.com/originals/3a/ff/c6/3affc64b2a60c260accc5110b0183035.jpg")
+
+
+shogun_assassin = videos.Movies("Shogun Assassin", "When the wife of the Shogun's Decapitator is murdered and he is \
+ordered to commit suicide by the paranoid Shogun, he and his four-year-old son escape and become assassins for hire,\
+embarking on a journey of blood and violent death", "1h 25min", "https://www.youtube.com/watch?v=bFOjbAQmIjY", "https://www.limitedruns.com/media/images/productimage-picture-shogun-assassin-1146.JPG")
+
+the_36_chambers_of_shaolin = videos.Movies("The 36 Chambers of Shaolin", "A man studies kung fu at the Shaolin Temple to fight back against the oppressive Manchu government.",
+                                           " 1h 55min", "https://www.youtube.com/watch?v=65GQtH6pzTY", "http://www.cityonfire.com/wp-content/uploads/2011/03/600full-the-36th-chamber-of-shaolin-poster.jpg")
+
+
+return_to_the_36th_chamber = videos.Movies("Return to the 36th Chamber", "A mistreated millworker learns martial arts to fight back against his bosses", "1h 39min", "https://www.youtube.com/watch?v=kOlwlSvK8Y4",
+                                           "http://www.gstatic.com/tv/thumb/movieposters/6586/p6586_p_v8_aa.jpg")
+
+
+the_shaolin_drunken_monk = videos.Movies("The Shaolin Drunken Monk", "The son of a martial arts teacher comes under\
+ the tutelage of a hermit who instructs him in the ways of Drunken Kung Fu", "1h 23min ", "https://www.youtube.com/watch?v=nc9D1Yoj7dA", "https://sep.yimg.com/ay/yhst-61160669843094/shaolin-drunken-monk-3.gif")
+
+movies = [shaolin_vs_wutang, mystery_of_chessboxing, shogun_assassin,
+          the_36_chambers_of_shaolin, return_to_the_36th_chamber, the_shaolin_drunken_monk]
+fresh_tomatoes.open_movies_page(movies)
---
+++ project4/videos.py
@@ -0,0 +1,37 @@
+import webbrowser
+
+
+class Videos(object):
+    """ Class to create video objects for movies and shows """
+    vVALID_RATING = ["G", "PG", "PG-13", "R"]
+
+    def __init__(self, title, plot, duration):
+        self.title = title
+        self.plot = plot
+        self.duration = duration
+
+    def Can_I_Stream(self):
+        """ Function to check if video is available on any streaming sites"""
+        webbrowser.open("https://www.justwatch.com/us/search?q=" + self.title)
+
+    def show_trailer(self):
+        """Function to play trailer or lip form youtube or other service"""
+        webbrowser.open(self.trailer)
+
+
+class Movies(Videos):
+    """ Class to hold movie object child of Videos"""
+
+    def __init__(self, title, plot, duration, trailer_youtube_url, poster_image_url):
+        super(Movies, self).__init__(title, plot, duration)
+        self.trailer_youtube_url = trailer_youtube_url
+        self.poster_image_url = poster_image_url
+
+
+class Tv_Shows(Videos):
+    """ Class to hold tv show objects child of Videos"""
+
+    def __init__(self, title, plot, duration, trailer_youtube_url, poster_image_url, ):
+        super(Tv_Shows, self).__init__(title, plot, duration)
+        self.trailer_youtube_url = trailer_youtube_url
+        self.poster_image_url = poster_image_url
