8a9fb5e49f5b5e089b7fd685e5f4ecacdf11ac6d
ksuber <k_suber52@yahoo.com>
2018-02-07 03:01:36 -0500

    added more coments

--- project4/videos.py
+++ project4/videos.py
@@ -2,7 +2,22 @@ import webbrowser
 
 
 class Videos(object):
-    """ Class to create video objects for movies and shows """
+     """Creates an intialized video object.
+    Input:
+        title, plot and duration.
+    Behavior:
+        Creates a video object.
+    Attributes:
+        Title (str): Movie Title
+        plot (str): Movie Plot.
+        duration (str): Movie length.
+        
+    Returns:
+        Intialized video object.
+    Methods:
+        Can_I_Stream (str): Opens browser to video streaming check site.
+        show_trailer (str) Opens browser to youtube to play trailer
+    """
     vVALID_RATING = ["G", "PG", "PG-13", "R"]
 
     def __init__(self, title, plot, duration):
@@ -12,16 +27,51 @@ class Videos(object):
         self.duration = duration
 
     def Can_I_Stream(self):
-        """ Function to check if video is available on any streaming sites"""
+       """Open the webbroser with a link to streaming video site.
+    Input:
+        None.
+    Behavior:
+        Opens the default webbroswer using self.title plus www.justwatch.com
+        to display a site showing if the movie or show is availbl.
+    Returns:
+        None.
+
+    """
         webbrowser.open("https://www.justwatch.com/us/search?q=" + self.title)
 
     def show_trailer(self):
-        """Function to play trailer or lip form youtube or other service"""
+        """Open the webbroser with the movie trailer.
+    Input:
+        None.
+    Behavior:
+        Opens the default webbroswer using self.trailer_youtube_trailer
+        to display the movie trailer.
+    Returns:
+        None.
+
+    """
         webbrowser.open(self.trailer)
 
 
 class Movies(Videos):
-    """ Class to hold movie object child of Videos"""
+   """Creates an intialized Movie object inherits from Parent class Videos .
+    Input:
+        title, plot,duration, trailer_youtube_url, poster_image_url.
+    Behavior:
+        Creates a Movie object.
+    Attributes:
+        Title (str): Movie Title
+        plot (str): Movie Plot.
+        duration (str): Movie length.
+        trailer_youtube_url (str) Link to trailer on Youtube
+        poster_image_url (str) Link to url for poster image
+        
+    Returns:
+        Intialized Movie object.
+    Methods:
+        Can_I_Stream (str): Opens browser to video streaming check site.
+        show_trailer (str) Opens browser to youtube to play trailer
+    """
 
     def __init__(self, title, plot, duration, trailer_youtube_url, poster_image_url):
         ''' Inherents attributes for Video and sets trailer and poster '''
@@ -31,8 +81,24 @@ class Movies(Videos):
 
 
 class Tv_Shows(Videos):
-    """ Class to hold tv show objects child of Videos"""
-
+      """Creates an intialized Tv show object inherits from Parent class Videos .
+    Input:
+        title, plot,duration, trailer_youtube_url, poster_image_url.
+    Behavior:
+        Creates a Movie object.
+    Attributes:
+        Title (str): Movie Title
+        plot (str): Movie Plot.
+        duration (str): Movie length.
+        trailer_youtube_url (str) Link to trailer on Youtube
+        poster_image_url (str) Link to url for poster image
+        
+    Returns:
+        Intialized Tv show object.
+    Methods:
+        Can_I_Stream (str): Opens browser to video streaming check site.
+        show_trailer (str) Opens browser to youtube to play trailer
+    """
     def __init__(self, title, plot, duration, trailer_youtube_url, poster_image_url, ):
         ''' Inherents attributes for Video and sets trailer and poster '''
         super(Tv_Shows, self).__init__(title, plot, duration)
